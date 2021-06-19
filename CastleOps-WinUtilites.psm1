c3795905c388f41db46c8b369f803b8d08436e11
503ace <bobbyfab1@hotmail.com>
2021-06-19 07:11:24 -0700

    Introduced XML as a config file

--- CastleOps-WinUtilites.psm1
+++ CastleOps-WinUtilites.psm1
@@ -6,8 +6,6 @@ function update-VM
 	param
 	([string]$Host)
 	
-	Import-Module WindowsUpdate
-	
 	try
 	{
 		Enter-PSSession -ComputerName $Host
@@ -15,5 +13,47 @@ function update-VM
 	catch
 	{
 		Write-Host $Host "Failed to connect" -ForegroundColor Red
+	}
+}
+
+function Read-cfg
+{
+	param
+	([string]$FilePath)
+	
+	if (Test-Path $FilePath)
+	{
+		return [xml](Get-Content $FilePath)
+	}
+	else
+	{
+		return 1
+	}
+}
+
+function Add-Host
+{
+	param
+	(
+		[string]$Path,
+		[string]$newHost
+	)
+	
+	if (Test-Path $Path)
+	{
+		$ConfigXML = [xml](Get-Content $Path)
+		
+		$newHostEntry = $ConfigXML.CreateElement("VM")
+		
+		$newHostnameElement = $newHostEntry.AppendChild($ConfigXML.CreateElement("HostName"))
+		$newHostnameElement.AppendChild($ConfigXML.CreateTextNode($newHost))
+		
+		$newHostEntryAdd = $ConfigXML.configuration.Hosts.AppendChild($newHostEntry)
+		
+		$ConfigXML.Save($Path)
+	}
+	else
+	{
+		return 1
 	}
 }
